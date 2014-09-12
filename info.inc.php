ffacec042caec21dd91ff1d63948fb34a9b5579c
bigs <mihai.nicolaescu@live.com>
2014-09-12 17:55:32 +0300

    Initial commit

---
+++ info.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Theme information
+ *
+ * @package    PhpMyAdmin-theme
+ * @subpackage PMA8165
+ */
+
+/**
+ * Icon set used by this theme: http://famfamfam.com/lab/icons/silk/
+ */
+
+$theme_name = 'pma8165';
+$theme_full_version = '1.0';
+?>
